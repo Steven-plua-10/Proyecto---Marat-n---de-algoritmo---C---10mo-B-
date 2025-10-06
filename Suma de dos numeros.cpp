ecb4759eb512ceca0a990a23e75876eb5091dc50
Steven-plua-10 <splua@alumno.montepiedra.edu.ec>
2025-10-06 13:01:57 -0500

    Add files via upload

---
+++ Suma de dos numeros.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Suma de dos numeros ===" << endl;
+    double num1, num2;
+    cout << "Ingresa el primer numero: ";
+    cin >> num1;
+    cout << "Ingresa el segundo numero: ";
+    cin >> num2;
+    cout << "La suma de " << num1 << " y " << num2 << " es " << num1 + num2 << endl;
+    return 0;
+}
