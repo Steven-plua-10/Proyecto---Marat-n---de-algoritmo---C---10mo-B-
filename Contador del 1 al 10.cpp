f884418e620fe24432e7403d96d32bd7b523054b
Steven-plua-10 <splua@alumno.montepiedra.edu.ec>
2025-10-06 13:08:49 -0500

    Add files via upload

---
+++ Contador del 1 al 10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Contador del 1 al 10 ===" << endl;
+    int i = 1;
+    while (i <= 10) {
+        cout << "Numero: " << i << endl;
+        i++;
+    }
+    return 0;
+}
